98273f0e72063b042600b71c818016f5acead3a3
Faunja <129631000+Faunja@users.noreply.github.com>
2025-03-07 08:57:57 -0500

    Updated files via upload
    Made it so that the snake, along with its tail, is properly displayed onto the screen.

--- background.py
+++ background.py
@@ -14,41 +14,35 @@ def show_mouseposition():
 	rect = pygame.Rect(xPosition, yPosition, Grid.cellSize, Grid.cellSize)
 	pygame.draw.rect(screen, White, rect)
 
-def show_snakepart(xPosition, yPosition, direction):
+def show_head(xPosition, yPosition):
 	size = (Grid.cellSize / 10, Grid.cellSize * (9 / 10))
-	if direction == (0, -1):
-		topRight = (xPosition + size[0], yPosition + size[0])
-		bottomRight = (xPosition + size[0], yPosition + Grid.cellSize)
-		bottomLeft = (xPosition + size[1], yPosition + Grid.cellSize)
-		topLeft = (xPosition + size[1], yPosition + size[0])
-	if direction == (1, 0):
-		topRight = (xPosition + size[0], yPosition + size[0])
-		bottomRight = (xPosition + size[0], yPosition + size[1])
-		bottomLeft = (xPosition + Grid.cellSize, yPosition + size[1])
-		topLeft = (xPosition + Grid.cellSize, yPosition + size[0])
-	if direction == (0, 1):
-		topRight = (xPosition + size[0], yPosition)
-		bottomRight = (xPosition + size[0], yPosition + size[1])
-		bottomLeft = (xPosition + size[1], yPosition + size[1])
-		topLeft = (xPosition + size[1], yPosition)
-	if direction == (-1, 0):
-		topRight = (xPosition, yPosition + size[0])
-		bottomRight = (xPosition, yPosition + size[1])
-		bottomLeft = (xPosition + size[1], yPosition + size[1])
-		topLeft = (xPosition + size[1], yPosition + size[0])
-		
+
+	topRight = (xPosition + size[0], yPosition + size[0])
+	bottomRight = (xPosition + size[0], yPosition + size[1])
+	bottomLeft = (xPosition + size[1], yPosition + size[1])
+	topLeft = (xPosition + size[1], yPosition + size[0])
+
+	return (topRight, bottomRight, bottomLeft, topLeft)
+
+def show_segment(xPosition, yPosition, direction):
+	size = (Grid.cellSize / 10, Grid.cellSize * (9 / 10))
+	adjustSize = (direction[0][0] * Grid.cellSize, direction[0][1] * Grid.cellSize, direction[1][0] * Grid.cellSize, direction[1][1] * Grid.cellSize)
+
+	topRight = (xPosition + size[1] + adjustSize[1], yPosition + size[0] - adjustSize[2])
+	bottomRight = (xPosition + size[1] + adjustSize[1], yPosition + size[1] + adjustSize[3])
+	bottomLeft = (xPosition + size[0] - adjustSize[0], yPosition + size[1] + adjustSize[3])
+	topLeft = (xPosition + size[0] - adjustSize[0], yPosition + size[0] - adjustSize[2])
+
 	return (topRight, bottomRight, bottomLeft, topLeft)
 
 def show_player():
 	headX = User.position[0] * Grid.cellSize
 	headY = User.position[1] * Grid.cellSize
-	pygame.draw.polygon(screen, Blue, show_snakepart(headX, headY, User.direction))
+	pygame.draw.polygon(screen, Blue, show_head(headX, headY))
 	for segment in User.tail:
 		segmentX = segment.position[0] * Grid.cellSize
 		segmentY = segment.position[1] * Grid.cellSize
-		pygame.draw.polygon(screen, Blue, show_snakepart(headX, headY, segment.frontDirection))
-		if segment.backDirection != False:
-			pygame.draw.polygon(screen, Blue, show_snakepart(headX, headY, segment.backDirection))
+		pygame.draw.polygon(screen, Blue, show_segment(segmentX, segmentY, segment.give_direction()))
 
 def display_background():
 	screen.fill(Black)
--- defineuser.py
+++ defineuser.py
@@ -2,7 +2,6 @@
 # By Kayden Campbell
 # Copyright 2025
 # Licensed under the terms of the GPL 3
-import pygame
 from variables import *
 from definegrid import Grid
 
@@ -12,15 +11,23 @@ def change_TrueFalse(variable):
 	return False
 
 class definetail:
-	def __init__(self, x, y, frontDirection, backDirection = False):
+	def __init__(self, x, y, direction):
 		self.position = (x, y)
-		self.frontDirection = frontDirection
-		self.backDirection = backDirection
+		self.direction = direction
 	
-	def update_tail(self, x, y, newDirection):
+	def update_tail(self, x, y, direction):
 		self.position = (x, y)
-		self.backDirection = self.frontDirection
-		self.frontDirection = newDirection
+		self.direction = direction
+
+	def give_direction(self):
+		if self.direction == (1, 0):
+			return ((0, 1), (0, 0))
+		elif self.direction == (-1, 0):
+			return ((1, 0), (0, 0))
+		elif self.direction == (0, 1):
+			return ((0, 0), (0, 1))
+		elif self.direction == (0, -1):
+			return ((0, 0), (1, 0))
 
 class defineuser:
 	def __init__(self):
@@ -29,45 +36,57 @@ class defineuser:
 		
 		self.position = [0, 0]
 		self.direction = (1, 0)
-		self.newDirection = (1, 0)
 		
-		self.points = 0
+		self.score = 0
+		self.tailSize = 15
 		self.tail = []
 		
-		self.speed = 4
+		self.speed = 6
 		self.time = 1
 
 	def change_showfps(self):
 		self.showfps = change_TrueFalse(self.showfps)
+	
+	def change_direction(self, x, y):
+		self.direction = (x, y)
 	
 	def check_wall_collision(self):
 		if 0 <= self.position[0] + self.direction[0] <= Grid.width - 1 and 0 <= self.position[1] + self.direction[1] <= Grid.height - 1:
 			return False
 		return True
 	
-	def change_direction(self, x, y):
-		self.newDirection = (x, y)
+	def check_tail_collision(self):
+		for segment in self.tail:
+			if self.position[0] + self.direction[0] == segment.position[0] + segment.direction[0] and self.position[1] + self.direction[1] == segment.position[1] + segment.direction[1]:
+				return True
+		return False
+	
+	def adjust_tail(self):
+		for currentTail in range(len(self.tail) - 1, -1, -1):
+			if len(self.tail) < self.tailSize + self.score:
+				self.tail.append(definetail(self.tail[currentTail].position[0], self.tail[currentTail].position[1], self.tail[currentTail].direction))
+			if currentTail != 0:
+				nextTail = self.tail[currentTail - 1]
+				self.tail[currentTail].update_tail(nextTail.position[0], nextTail.position[1], nextTail.direction)
+				continue
+			self.tail[currentTail].update_tail(self.position[0], self.position[1], self.direction)
+		if len(self.tail) == 0:
+			self.tail.append(definetail(self.position[0], self.position[1], self.direction))
 	
 	def shift_position(self):
-		self.direction = self.newDirection
-		if self.check_wall_collision() == True:
-			return
 		self.position[0] += self.direction[0]
 		self.position[1] += self.direction[1]
 	
-	def check_tail(self):
-		if len(self.tail) == 0:
-			self.tail.append(definetail(self.position[0], self.position[1], self.direction))
+	def movement(self):
+		if self.check_wall_collision() == True or self.check_tail_collision() == True:
 			return
-		for segment in range(len(self.tail)):
-			if segment == len(self.tail) - 1:
-				self.tail[segment].update_tail(self.position[0], self.position[1], self.newDirection)
-	
+		self.adjust_tail()
+		self.shift_position()
+
 	def check_movement(self):
 		if self.time >= FPS / self.speed:
 			self.time = 1
-			self.check_tail()
-			self.shift_position()
+			self.movement()
 			return
 		self.time += 1
 
